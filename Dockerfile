fa612cca9bf4567269625a884de3f0ba98799fee
Abdelhak MEKAOUI <mekaouiabdelhak1@gmail.com>
2024-04-30 22:23:37 +0100

    adding mongo loader

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Set the environment variable for MongoDB URI
+ENV MONGO_URI=""
+
+# Expose the port number the app runs on
+EXPOSE 8000
+
+# Run the application
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ main.py
@@ -0,0 +1,95 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from transformers import  pipeline, AutoTokenizer, AutoModel
+import nest_asyncio
+from langchain_community.document_loaders.mongodb import MongodbLoader
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+import os
+import torch
+
+load_dotenv()
+nest_asyncio.apply()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Load the model locally
+model_id = "TinyLlama-1.1B-Chat-v1.0"
+model_path = f"./data/{model_id}"
+cahce_path = './cache'
+
+
+model = AutoModel.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+pipe = pipeline("text-generation",
+                model=model,
+                tokenizer=tokenizer,
+                model_kwargs={"cache_dir": cahce_path, 'device_map': 'auto' , "torch_dtype" : torch.bfloat16, "max_length" : 2000},
+                max_new_tokens=256,  do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+
+hf = HuggingFacePipeline(model_id=model_id, pipeline=pipe)
+
+hf.pipeline.model._validate_model_kwargs = lambda self: None
+
+
+
+
+uri = "mongodb://root:password@localhost:27017"
+# Setup MongoDB loader
+loader = MongodbLoader(
+    connection_string=uri,#os.getenv('MONGO_URI'),
+    db_name="chat-docs",
+    collection_name="docs",
+)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Define the prompt using special tokens that your model understands
+template = """<|system|>Repond selon le contexte : {context} </s> <|user|>{question} Repond en français</s> <|assistant|>"""
+prompt = ChatPromptTemplate.from_template(template)
+
+class ChatQuestion(BaseModel):
+    chatQuestion: str
+
+@app.post("/v1/")
+async def create_item(item: ChatQuestion):
+
+    print("processing ...")
+    print(item)
+    data = loader.load_and_split()
+    embeddings = HuggingFaceEmbeddings()
+    vectorstore = FAISS.from_documents(data, embeddings)
+    retriever = vectorstore.as_retriever()
+
+    chain = (
+    {"context": retriever , "question": RunnablePassthrough()}
+    | prompt
+    | hf
+    | StrOutputParser()
+    )    
+    response = chain.invoke(item.chatQuestion)
+    
+    print("end processing")
+    return {"chatResponse": response}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
